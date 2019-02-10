9a4224053230b3d8b9ac6a1b5e56eec98b7476f4
StanimalTheMan <sjc591@nyu.edu>
2019-02-10 16:56:13 -0500

    Update implementation of explanation

--- seasonalTheme.js
+++ seasonalTheme.js
@@ -48,13 +48,45 @@ and ends on
 Saturday, December 21
 All dates are in Eastern Time.
     */
+
+    //switch statements etc. are welcome <CONTRIBUTIONS ARE WELCOME>
     let date = new Date();
     let hour = date.getHours()
     let minute = date.getMinutes();
     let month = date.getMonth();
     let day = date.getDate();  // get day of month (1-31) of specified date according to local time according to MDN
-    if (month < 3) {
+    if (month  < 3) {
         setSeasonalTheme('winter');
+    } else if (month == 3) {
+        if (day < 20) {
+            setSeasonalTheme('winter');
+        } else {
+            setSeasonalTheme('spring');
+        }
+    } else if (month > 3 && month < 6) {
+        setSeasonalTheme('spring');
+    } else if (month == 6) {
+        if (day < 21) {
+            setSeasonalTheme('spring');
+        } else { //add to make it work for exact time etc. <CONTRIBUTION>
+            setSeasonalTheme('summer');
+        }
+    } else if (month > 6 && month < 9) {
+        setSeasonalTheme('fall');
+    } else if (month == 9) {
+        if (day < 23) {
+            setSeasonalTheme('summer');
+        } else {
+            setSeasonalTheme('fall');
+        }
+    } else if (month > 9 && month < 12) {
+        setSeasonalTheme('fall');
+    } else if (month == 12) {
+        if (day < 21) {
+            setSeasonalTheme('fall');
+        } else {
+            setSeasonalTheme('winter');
+        }
     }
 }
 
